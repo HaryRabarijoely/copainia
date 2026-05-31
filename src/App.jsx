1c45d85ac544025d3438e89890429865f7884dfb
haryrabarijoely <rabonia@hotmail.com>
2026-05-31 21:45:15 +0200

    adding chat page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
+import Chat from "./pages/Chat";
+
 export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Mon Copain FabiGPT
-      </h1>
-    </div>
-  );
+  return <Chat />;
 }
